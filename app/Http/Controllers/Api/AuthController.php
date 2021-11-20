e8025c1e47e4150f9a094e338392207bc0dcb642
abdallahsultan <abdallahsultan1996@gmail.com>
2021-11-20 23:48:35 +0200

    sadasdsa

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -136,7 +136,7 @@ class AuthController extends ApiBaseController
         return response()->json([
             'access_token' => $token,
             'token_type' => 'bearer',
-            'expires_in' =>  auth('api')->factory()->getTTL() * 60
+            'expires_in' =>  auth('api')->factory()->getTTL() * 7300
         ]);
     }
     protected function payload()
